70f95c82797354acce3f7fb31cb3715fb2a1c75f
KarinaKhazanova <karinakhazanova@gmail.com>
2022-10-19 15:33:39 +0300

    day2

---
+++ week2day2/Exercise2/script.js
@@ -0,0 +1,12 @@
+let newDog = "Chihuahua";
+
+console.log(newDog.length);
+console.log(newDog.toUpperCase());
+console.log(newDog.toLowerCase());
+
+if (newDog) {
+    console.log("I love Chihuahuas, it’s my favorite dog breed");
+} else {
+    console.log("I dont care, I prefer cats");
+}
+
