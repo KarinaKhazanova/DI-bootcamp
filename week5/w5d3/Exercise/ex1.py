80b244219467d13396d40b53c4748883e831b93f
KarinaKhazanova <karinakhazanova@gmail.com>
2022-11-08 20:46:55 +0200

    day3

---
+++ week5/w5d3/Exercise/ex1.py
@@ -0,0 +1,17 @@
+number = -5
+absolute_number = abs (number)
+print (absolute_number)
+
+
+y = 6
+x = int (y)
+print (x)
+
+
+name = input ("What is your name")
+print(name)
+
+class Explanation:
+    "abs - convert number to absolute, int-to integer, input-is for users input"
+
+print(Explanation.__doc__)
---
+++ week5/w5d3/Exercise/ex2.py
@@ -0,0 +1,10 @@
+class Currency:
+    def __init__(self, currency, amount):
+        self.currency = currency
+        self.amount = amount
+
+
+c1 = Currency('dollar', 5)
+
+print(c1)
+
