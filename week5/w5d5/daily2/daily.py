5b48733b3c1b177126468b92fc7a186cd5cc3c9e
KarinaKhazanova <karinakhazanova@gmail.com>
2022-11-12 13:39:50 +0200

    mini

---
+++ week5/w5d5/daily2/daily.py
@@ -0,0 +1,5 @@
+#A class is a code template for creating objects
+#Instance is an object that belongs to a class
+#Encapsulation is a mechanism of wrapping the data (variables) and code acting on the data (methods) together as a single unit
+#Abstraction in python is defined as a process of handling complexity by hiding unnecessary information from the user
+#Inheritance is a feature that allows us to define a class that inherits all the methods and properties from another class
