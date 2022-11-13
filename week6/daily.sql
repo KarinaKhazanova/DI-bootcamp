206e90db1323d4d2a52f08911982b8f3d277bba2
KarinaKhazanova <karinakhazanova@gmail.com>
2022-11-13 21:26:08 +0200

    daily

---
+++ week6/daily.sql
@@ -0,0 +1,33 @@
+-- Database: hollywood
+
+-- DROP DATABASE IF EXISTS hollywood;
+
+CREATE DATABASE hollywood
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+	
+	-- CREATE TABLE actors(
+--  actor_id SERIAL PRIMARY KEY,
+--  first_name VARCHAR (50) NOT NULL,
+--  last_name VARCHAR (100) NOT NULL,
+--  age DATE NOT NULL,
+--  number_oscars SMALLINT NOT NULL
+-- );
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Matt','Damon','08/10/1970', 5);
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('George','Clooney','06/05/1961', 2);
+-- select * from actors;
+
+select COUNT(first_name) from actors;
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Brad','Pitt','', 1);
