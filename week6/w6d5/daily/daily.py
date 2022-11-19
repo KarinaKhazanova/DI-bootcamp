f187388d53760bcfa74f4b4cf925fe1c8eb5b039
KarinaKhazanova <karinakhazanova@gmail.com>
2022-11-19 20:40:48 +0200

    day5

---
+++ week6/w6d5/daily/daily.py
@@ -0,0 +1,57 @@
+import requests
+import psycopg2
+
+url = 'https://restcountries.com/v3.1/all?fields=name,capital,flag,subregion,population'
+
+data = requests.get(url).json()
+# print(type(data))​
+# # print (data [0]['name']['common'])
+
+# countries_names = [country['name']['common'] for country in data]
+
+# print(countries_names)
+
+print (data [0])
+name = data[0]['name']['common']
+capital = data[0]['capital'][0]
+subregion = data[0]['subregion']
+flag = data[0]['flag']
+population = data[0]['population']
+​
+print (name,capital,subregion,flag,population)
+​
+​
+# connect:
+​
+​
+HOSTNAME =  'localhost'
+USERNAME =  'postgres'
+PASSWORD =  '11111'
+DATABASE =  'country'
+​
+connection = psycopg2.connect(host =HOSTNAME, user =USERNAME, password = PASSWORD, dbname =DATABASE)
+
+cursor = connection.cursor()
+q = "SELECT * from countries;" 
+
+cursor.execute(q)
+
+results = cursor.fetchall()
+print(results)
+
+# connection.close()
+
+
+# insert:
+
+cursor = connection.cursor()
+
+new_row = ('Israel', 'IL', 'Middle Eas', 12345, 8)
+
+q = f"INSERT INTO countries (name, capital, sub, flag, population)values {new_row};"
+
+cursor.execute(q)
+
+connection.commit()
+
+connection.close()
